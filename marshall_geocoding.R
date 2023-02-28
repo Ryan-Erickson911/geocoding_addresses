837f71f80e0db5c72016d0f8f8a58e4fa662eceb
Ryan Erickson <ryanerickson407@gmail.com>
2023-02-28 14:29:51 -0700

    fixed map

--- marshall_geocoding.R
+++ marshall_geocoding.R
@@ -5,75 +5,74 @@ library(ggplot2)
 library(ggspatial)
 
 #function junction
-marshall_map = function(sf_point_object, field, title, Stitle, Ltitle) {
-  ggplot(sf_point_object) + 
+change_display = function(category) {
+  ggplot(geo_marshall) + 
     annotation_map_tile() +
     annotation_scale() +
-    geom_sf(aes(color=sf_point_object[[field]])) + 
+    geom_sf(aes(color=impact_cat)) + 
     scale_color_viridis_d(option = "B")+
-    ggtitle(title, subtitle = Stitle)  + 
+    ggtitle("Marshall Fire Survey Results", subtitle = paste0("Total Respondants Displayed: ",nrow(geo_marshall))) + 
     theme(plot.title = element_text(hjust=0.5),
-          plot.subtitle = element_text(hjust=0.5)) + 
-    guides(colour=guide_legend(title=Ltitle)) 
+          plot.subtitle = element_text(hjust=0.5)) +
+    guides(colour=guide_legend(title="Impact Level")) + 
+    gghighlight::gghighlight(impact_cat==category)
 }
-#Win computer - R:/Marshall Fire Health/marshall_w1.csv
-data1<-read.csv("/Volumes/research/Marshall Fire Health/marshall_w1.csv", sep=",")
+###need to connect to UCB research drive
+#Win - R:/Marshall Fire Health/
+#mac - /Volumes/research/Marshall Fire Health/
+m_addresses<-read.csv("marshall_w1.csv", sep=",")
 
+##addys to fix
+#553 GRANT ST == 553 GRANT AVE
+m_addresses[m_addresses$mailingaddr1=="553 GRANT ST",]$mailingaddr1 = "553 GRANT AVE"
+ 
 #geocoding - assiging lats and longs via Google API - will need key
-geo_marshall = data1 %>% 
+geo_marshall = m_addresses %>% 
   mutate(full_address = paste0(.$mailingaddr1,", ",.$mailingcity,", ", .$mailingstate,", ",.$mailingzip)) %>% 
-  mutate_geocode(full_address, output="latlona")
+  mutate_geocode(full_address, output="latlona") %>%
+  filter(!is.na(lat)) %>% 
+  filter(impact_cat != "") %>% 
+  st_as_sf(coords = c("lon", "lat"),  crs = 4326) %>%
+  st_filter(den_co, .predicate=st_within)
 #NA lat and long - one weird one
-what_this = geo_marshall[is.na(geo_marshall$age),]
-#553 Grant Ave - Fix Address
+#what_this = geo_marshall[is.na(geo_marshall$age),]
+
 #mapping - only for points in CO
-co_co = st_read("C:/github/geocoding_addresses/aqi_kriging-main/co_counties/geo_export_ecf55ab8-2f88-4a1f-b4fb-b21f58fb9432.shp")
+co_co = st_read("C:/Users/RErickson/Documents/GitHub/geocoding_addresses/aqi_kriging-main/co_counties/geo_export_ecf55ab8-2f88-4a1f-b4fb-b21f58fb9432.shp")
 
-# Select only Denver metro counties and geometry - might be able to narrow down to CDP if wanted
-
+# Select only Denver metro counties and geometry
 den_co = co_co %>%
   filter(county %in% c("BOULDER")) %>%
   select(county, geometry) %>% 
-  st_as_sf(crs=4326)
-st_crs(geo_marshall)
-#raw survey data
-marshall_gc = geo_marshall %>%
-  filter(!is.na(lat)) %>% 
-  st_as_sf(coords = c("lon", "lat"),  crs = 4326) %>% 
-  st_filter(den_co)
+  st_transform(crs=st_crs(geo_marshall))  
 
-#cleaned data (no NA in impact_cat)
-marshall_gc_clean = marshall_gc %>%
-  filter(impact_cat != "") %>% 
-  st_as_sf(coords = c("lon", "lat"),  crs = 4326) %>% 
-  st_filter(den_co, .predicate=st_within)
-
-#template map - raw data, boulder county limits included
+#template map - raw data, boulder county limits included, zoomed out
 ggplot(den_co) + 
   annotation_map_tile() +
   annotation_scale() +
   geom_sf(fill=NA) +
 #  geom_sf_text(aes(label=county))+ for mapping multiple counties...maybe
-  geom_sf(data=marshall_gc, aes(color=impact_cat)) #+ lims(y = c(39.9, 40.1), x = c(-105.35, -105)) 
+  geom_sf(data=geo_marshall, aes(color=impact_cat))
 
-ggplot(marshall_gc) + 
+#Final Map
+ggplot(geo_marshall) + 
   annotation_map_tile() +
   annotation_scale() +
-  geom_sf(aes(color=impact_cat)) + 
-  scale_color_viridis_d(option = "B")+
-  ggtitle("Marshall Fire Survey Results", subtitle = "Zoomed") + 
-  guides(colour=guide_legend(title="Impact Level")) #+ gghighlight::gghighlight("")
-
-##Final Map##
-ggplot(marshall_gc_clean) + 
-  annotation_map_tile() +
-  annotation_scale() +
-  geom_sf(aes(color=impact_cat)) + #everything after this line is just making the map noice
-  scale_color_viridis_d(option = "B")+
-  ggtitle("Marshall Fire Survey Results", subtitle = "Zoomed") + 
+  geom_sf(aes(color=impact_cat)) +
+  scale_color_manual(values = c("Complete loss" = "#d7191c", 
+                                "Damaged, living there" = "#fdae61",
+                                "Damaged, not living there" = "#ffffbf",
+                                "No damage, living there" = "#a6d96a",
+                                "No damage, not living there" = "#1a9641")) +
+  ggtitle("Marshall Fire Survey Results", subtitle=paste0("Total Respondants Displayed: ",nrow(geo_marshall))) + 
   theme(plot.title = element_text(hjust=0.5),
-        plot.subtitle = element_text(hjust=0.5)) + 
-  guides(colour=guide_legend(title="Impact Level")) #+ gghighlight::gghighlight(impact_cat == "Complete loss")
-
-#function testing
-marshall_map(marshall_gc_clean, "impact_cat", title = "Marshall Fire Survey Results", Ltitle = "Impact Level", Stitle = "Zoomed")
+        plot.subtitle = element_text(hjust=0.5)) +
+  guides(colour=guide_legend(title="Impact Level"))
+#for easy highlighting
+func = unique(geo_marshall$impact_cat) #get specific damage types (codebook)
+#1 = "Damaged, living there"   
+#2 = "Complete loss"         
+#3 = "Damaged, not living there"
+#4 = "No damage, living there"  
+#5 = "No damage, not living there"
+change_display(func[1])
